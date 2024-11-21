d6d94f464a50463d8a024c22ffb78dbb427c2382
EC2 Default User <ec2-user@ip-172-31-34-8.us-west-2.compute.internal>
2024-11-21 00:36:18 +0000

    test

--- main.tf
+++ main.tf
@@ -14,7 +14,7 @@ terraform {
 
 resource "aws_s3_bucket" "example" {
   bucket = "newnewtest" # 고유한 버킷 이름 생성
-  acl    = "private" 
+  acl    = "private"
 
   tags = {
     Name        = var.bucket_tag_name
